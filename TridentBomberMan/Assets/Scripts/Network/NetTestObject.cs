2cf55ce59697ccecfe9c0c8484807aa2dd676498
koshi <koshi.rky@gmail.com>
2017-10-20 13:26:02 +0900

    ネットワーク接続時の誤差補間処理テスト

---
+++ TridentBomberMan/Assets/Scripts/Network/NetTestObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetTestObject : Photon.PunBehaviour {
+
+    PhotonView _photonView;
+	// Use this for initialization
+	void Start () {
+        _photonView = GetComponent<PhotonView>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!_photonView.isMine) return;
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 5;
+        float y = Input.GetAxis("Vertical") * Time.deltaTime * 5;
+        transform.position += new Vector3(x, y, 0);
+	}
+}
